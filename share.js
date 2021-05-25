fac46949ae3650f79212997a8315e087766e0805
spdfEdu <71858133+spdfEdu@users.noreply.github.com>
2021-05-25 19:10:53 +0530

    Update share.js

--- share.js
+++ share.js
@@ -4,3 +4,8 @@ shareButton.addEventListener("click", () => share())
        navigator.share({
        text: document.getElementById("Name").textContent+"  "
              +window.location.href })}}
+function addCommentBox() {
+var i = document.getElementById('comments');
+var d = document.getElementById('displace-comments');
+d.appendChild(i);
+}
