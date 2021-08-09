0f75a138cca2359b3864ab2b773bc963012239fc
spdfEdu <71858133+spdfEdu@users.noreply.github.com>
2021-08-09 19:16:37 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,12 @@
-//shareButton
+ //shareButton
    function share() {
    if (navigator.share) {
        navigator.share({
        text: document.getElementById("Name").textContent+"  "
              +window.location.href })}
    else {
-     window.open('https://wa.me/?text=*'+document.getElementById("Name").textContent+'*%0A'+window.location.href)
-   }
-   }
+     window.open('https://wa.me/?text=*'+document.getElementById("Name").textContent+'*%0A'+window.location.href)}};
 
-//comment Box
-function addCommentBox() {
-var i = document.getElementById('comments');
-var d = document.getElementById('displace-comments');
-d.appendChild(i);
-}
-
-//Contact Us mail Url
- document.getElementById("contact").setAttribute("onclick","location.href='mailto:eduspdf@gmail.com?subject=spdfEdu%20Website%20Queries&body=Name%20%3A%20%0D%0AWebsite%20%3A%20%0D%0AEmail%20Id%20%3A%20%0D%0AContact%20No.%20%3A%20%0D%0A%0D%0AMessage%20%3A%20'");
- 
  //header section
 var header = document.createElement("div");
 header.innerHTML =
@@ -33,6 +21,13 @@ header.innerHTML =
 var child = document.createElement("div");
 child.innerHTML =
  `<div id="boxFooter">
+<div id="copyright"><hr>
+<p id="copyrightHead">
+    <b> DMCA Disclaimer </b></p> We are not hosting any copyrighted contents on our servers, it’s a catalog of links that already found on the internet. Check out our 
+<div id="DMCA" onclick="location.href='https://spdfedu.blogspot.com/p/dcma.html'">DMCA Policy</div>.<b>If you feel that we have violated your copyrights, then please 
+<div id="contact" onclick="location.href='mailto:eduspdf@gmail.com?subject=spdfEdu%20Website%20Queries&body=Name%20%3A%20%0D%0AWebsite%20%3A%20%0D%0AEmail%20Id%20%3A%20%0D%0AContact%20No.%20%3A%20%0D%0A%0D%0AMessage%20%3A%20'">contact us</div>
+immediately, concerned content will be taken down within 24 hours.</b><br><br></div>
+ 
   <div id="boxFoot">
   <div class="foot" onclick="location.href='mailto:eduspdf@gmail.com?subject=spdfEdu%20Website%20Queries&body=Name%20%3A%20%0D%0AWebsite%20%3A%20%0D%0AEmail%20Id%20%3A%20%0D%0AContact%20No.%20%3A%20%0D%0A%0D%0AMessage%20%3A%20'">
     Contact Us 
