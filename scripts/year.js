956cee9bc026c9b61a76896ca8f5efbdabd1607c
spdfEdu <71858133+spdfEdu@users.noreply.github.com>
2021-10-08 11:26:42 +0530

    Create year.js

---
+++ scripts/year.js
@@ -0,0 +1,105 @@
+//footer section
+var child = document.createElement("div");
+child.innerHTML =
+ `<div id="boxFooter">
+<div id="copyright"><hr>
+<p id="copyrightHead">
+    <b> DMCA Disclaimer </b></p> We are not hosting any copyrighted contents on our servers, it’s a catalog of links that already found on the internet. Check out our 
+<div id="DMCA" onclick="location.href='https://spdfedu.blogspot.com/p/dcma.html'">DMCA Policy</div>.<b>If you feel that we have violated your copyrights, then please 
+<div id="contact" onclick="location.href='mailto:eduspdf@gmail.com?subject=spdfEdu%20Website%20Queries&body=Name%20%3A%20%0D%0AWebsite%20%3A%20%0D%0AEmail%20Id%20%3A%20%0D%0AContact%20No.%20%3A%20%0D%0A%0D%0AMessage%20%3A%20'">contact us</div>
+immediately, concerned content will be taken down within 24 hours.</b><br><br></div>
+  <div id="boxFoot">
+  <div class="foot" onclick="location.href='mailto:eduspdf@gmail.com?subject=spdfEdu%20Website%20Queries&body=Name%20%3A%20%0D%0AWebsite%20%3A%20%0D%0AEmail%20Id%20%3A%20%0D%0AContact%20No.%20%3A%20%0D%0A%0D%0AMessage%20%3A%20'">
+    Contact Us 
+  </div> |
+  <div class="foot" onclick="location.href='https://spdfedu.blogspot.com/'">
+   Home
+  </div> |
+  <div class="foot" onclick="location.href='https://spdfedu.blogspot.com/p/dcma.html'">
+    DCMA Policy
+  </div>
+  </div>
+  <div id="copySpdfedu">
+    Copyright &#169; 2021 spdfEdu
+  </div>
+  <div class="half-hr"></div>
+</div>`;
+document.body.append(child);
+
+// shareBox
+//finding box
+var insert = document.getElementById('box')
+
+//creating the share Box
+var url = 'Main Website --> bit.ly/spdfEdu';
+insert.insertAdjacentHTML('afterend',
+`<br><div id="prepBox">
+  <div id="prep" onclick="location.href='whatsapp://send?phone=919883553053&text=%E2%9C%A8%20%E2%84%99%E2%84%9D%F0%9D%94%BC%E2%84%99%F0%9D%95%83%F0%9D%94%B8%F0%9D%94%BB%F0%9D%94%BB%F0%9D%94%BC%E2%84%9D%20%F0%9D%95%86%F0%9D%94%BD%F0%9D%94%BD%F0%9D%94%BC%E2%84%9D%20%E2%9C%A8%0A%0A*1st%20Year%20Offer*%0A(Anatomy,%20Physiology,%20Biochemistry)%0A%0A%F0%9D%9F%8F%F0%9D%90%AC%F0%9D%90%AD%20%F0%9D%90%8F%F0%9D%90%AB%F0%9D%90%A8%F0%9D%90%9F%20-%20*%E2%82%B95600*%20/%206%20month%0A*%E2%82%B96000*%20/%209%20month%0A*%E2%82%B96300*%20/%201year%20(%20MRP%20~%E2%82%B99000~%20)%0A%0A*2nd%20Year%20Offer*%0A(Pathology,%20Pharmacology,%20Microbiology)%0A%0A%F0%9D%9F%90%F0%9D%90%A7%F0%9D%90%9D%20%F0%9D%90%8F%F0%9D%90%AB%F0%9D%90%A8%F0%9D%90%9F%20-%20*%E2%82%B96900*%20/%206%20month%20%0A*%E2%82%B97300*%20/%209%20month%20%0A*%E2%82%B97800*/%201%20year%20(%20MRP%20~%E2%82%B910800~%20)%0A%0A*Complete%20PrepLadder%E2%80%99s%20Pack*%0AIncludes%20Clinical%20Essentials,%20Updated%20Video%20Lectures,%20Integrated%20QBank,%20Treasures,%20NEXT%20/%20FMGE%20orientation%20and%20much%20more.%20Have%20you%20enrolled%20yet?%20Enroll%20now%20to%20get%0A%E2%9C%85%20%E2%82%B916,500%20Off%0A%E2%9C%85%201%20Month%20Extra%20Validity%20on%20subscriptions%20of%2012%20months%20and%20above%F0%9F%95%BA%0A%0A*4months:%20%E2%82%B916,999*%0A*6%20Months:%2023,499*%0A*9%20Months:%2026,499*%0A*12%20%2b%201%20Month:%2031,399*%0A*18%20%2b%201%20Month:%2037,399*%0A*24%20%2b%201%20Month:%2042,399*%0A*36%20%2b%201%20Month:%2047,399*%0A*48%20%2b%201%20Month:%2052,399*%0A*60%20%2b%201%20month:%2057,399*%0A*72%20%2b%201%20Month:%2062,399*%0A%0A*CREDIT%20CARD%20EMI%20FACILITIES%20AVAILABLE*%0APayment%20will%20be%20done%20in%20app.&app_absent=0'">
+<div id="prepImgBox">
+  <img id="prepImg" src="https://spdfedu.github.io/image/prepladder.jpg" alt="PrepLadder"/></div>
+ <div id="prepHead">
+ 𝔹𝔼𝕊𝕋 ℙℝ𝔼ℙ𝕃𝔸𝔻𝔻𝔼ℝ 𝕆𝔽𝔽𝔼ℝ𝕊
+</div><div id="offerDetails">
+<div class="proff"><div class="profH">
+    1st Proff</div><div class="cost">
+₹6300 / 1 year (<strike>₹9000</strike>)
+</div></div><div class="proff"><div class="profH"> 2nd Proff</div><div class="cost">₹7800 / 1 year (<strike>₹10800</strike>)</div></div><div class="proff"><div class="profH">Complete Pack</div>
+  <div class="cost"> ₹42,399 for (24 + 1 Month)</div><div class="cost">₹31,399 for (12 + 1 Month)</div></div>
+<div class="costMore"><b>6 months &amp; 9 months Packs also available<div id="costLink">To Avail / Know More<div class="knowMore">Click Here
+</div></div></b></div></div></div>
+</div><br>
+<br><div id="share-box">
+    <div id="head-share">
+      Do Share with your friends</div>
+   <div id="box">
+     <div class="item">
+  <div class="button wp" onclick="location.href='whatsapp://send?text=*'+document.title+'*%0A_'+window.location.href+'_%0A%0A'+url;" href=''>
+     <i id="wpSvg" class="sBs"></i>
+  </div></div>
+     <div class="item">
+  <div class="button sms" onclick="location.href='sms://?body='+document.title+'%0A'+window.location.href+'%0A%0A'+url" href=''>
+     <i id="smsSvg" class="sBs"></i>
+  </div></div>
+     <div class="item">
+  <div class="button cpy" id="copyButton">
+     <i id="cpySvg" class="sBs"></i>
+  </div></div>
+     <div class="item">
+  <div class="button mail" onclick="location.href='mailto:?subject='+document.title+'&body=Content%20Url:%20'+window.location.href+'%0A%0A'+url;" href=''>
+     <i id="mailSvg" class="sBs"></i>
+  </div></div>
+     <div class="item">
+  <div class="button shr" onclick="share()"> <i id="shrSvg" class="sBs"></i>
+  </div></div></div></div><br>
+  <div id="teleBox">
+  <div id="tele" onclick="location.href='https://t.me/spdfEdu'">
+   Join spdfEdu on Telegram &nbsp;&nbsp;
+  <i class="teleSvg"></i></div></div>
+`);
+//share function
+   function share() {
+   if (navigator.share) {
+       navigator.share({
+       text: document.title+"\n"
+             +window.location.href+"\n\n"+url})}
+   else {
+     window.open('whatsapp://send?text=*'+document.title+'*%0A_'+window.location.href+'_%0A%0A'+url)}}
+//copy Button function  
+  var Text = document.createElement('div');
+  Text.id = 'copyTry';
+  Text.innerHTML = document.title +'<br>' +window.location.href+'<br><br>'+url;
+  document.getElementById('share-box').append(Text);
+  var copyBtn = document.getElementById('copyButton');
+copyBtn.addEventListener('click', function(event) {  
+  var text = document.getElementById('copyTry');
+  var range = document.createRange();  
+  range.selectNode(text);  
+  window.getSelection().addRange(range);
+  try { var successful = document.execCommand('copy');
+  } catch(err) { console.log('Oops, unable to copy') }  
+  window.getSelection().removeAllRanges(); 
+});
+//replacing error image
+{var i;
+for( i=0; i<22; i++){
+ document.getElementsByClassName("cover")[i].setAttribute("onerror","this.className ='book-cover'");}}
