276ea3150d94fbe1b7814a6a6f43c4d0adc29832
spdfEdu <71858133+spdfEdu@users.noreply.github.com>
2022-01-11 11:08:44 +0530

    update tintin.js

--- scripts/tintin.js
+++ scripts/tintin.js
@@ -190,6 +190,41 @@ hfs.innerHTML =
 .teleSvg {height: 35px;
 content:url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' style='fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:1.41421;' viewBox='0 0 24 24'%3E%3Cpath fill='%230088cc' d='M12,0c-6.626,0 -12,5.372 -12,12c0,6.627 5.374,12 12,12c6.627,0 12,-5.373 12,-12c0,-6.628 -5.373,-12 -12,-12Zm3.224,17.871c0.188,0.133 0.43,0.166 0.646,0.085c0.215,-0.082 0.374,-0.267 0.422,-0.491c0.507,-2.382 1.737,-8.412 2.198,-10.578c0.035,-0.164 -0.023,-0.334 -0.151,-0.443c-0.129,-0.109 -0.307,-0.14 -0.465,-0.082c-2.446,0.906 -9.979,3.732 -13.058,4.871c-0.195,0.073 -0.322,0.26 -0.316,0.467c0.007,0.206 0.146,0.385 0.346,0.445c1.381,0.413 3.193,0.988 3.193,0.988c0,0 0.847,2.558 1.288,3.858c0.056,0.164 0.184,0.292 0.352,0.336c0.169,0.044 0.348,-0.002 0.474,-0.121c0.709,-0.669 1.805,-1.704 1.805,-1.704c0,0 2.084,1.527 3.266,2.369Zm-6.423,-5.062l0.98,3.231l0.218,-2.046c0,0 3.783,-3.413 5.941,-5.358c0.063,-0.057 0.071,-0.153 0.019,-0.22c-0.052,-0.067 -0.148,-0.083 -0.219,-0.037c-2.5,1.596 -6.939,4.43 -6.939,4.43Z'%3E%3C/path%3E%3C/svg%3E");
 }
+ #head-Books {
+      text-align: left;
+      font-weight: bold;
+      font-size: 20px;
+      font-family: 'Noto Sans JP';
+  }
+  .box-Books {
+      border: 1px solid #c5c5c5;
+      display: flex;
+      padding: 5px;
+      font-size: 18px;
+      border-top-left-radius: 20px;
+      user-select: none;
+      font-family: Sans-Serif;
+      background-color: rgb(254,255,250);
+}
+.box-Books:hover {
+      box-shadow: 1px 1px 7px 1px #55555588;
+}
+.cover-Books {
+      flex: 1;
+      width: 100px;
+      height: auto;
+      border: 0px;
+}
+.details-Books {
+      flex: 2;
+      margin-left: 15px;
+      font-size: 16px;
+}
+.head-box-Books {
+      font-size: 18px;
+      font-weight: bold;
+      line-height: 1.5;
+}
  </style>
   </div><br><br>
   
@@ -197,6 +232,33 @@ content:url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' style=
   <div id="tele" onclick="location.href='https://t.me/tintinPDFs'">
    Connect with us on Telegram&nbsp;&nbsp;
   <i class="teleSvg"></i></div></div>
+  <br><br>
+
+<div id="moreBooks">
+    <h3 id="head-Books">
+      You may also view:
+    </h3><br>
+   <div class="box-Books" onclick="location.href='https://bit.ly/ncert-bengali'">
+      <img class="cover-Books" src="https://spdfedu.github.io/image/ncert-bengali.jpg" alt="NCERT Books in Bengali | spdfEdu" />
+      <div class="details-Books">
+        <div class="head-box-Books">
+       NCERT in Bengali
+      </div>
+        All NCERT Books PDF in Bengali are available here. <br />
+     From Class I to XII.
+      </div>
+    </div><br>
+    <div class="box-Books" onclick="location.href='https://bit.ly/medBooks'">
+      <img class="cover-Books" src="https://spdfedu.github.io/image/medBooks-sm.png" alt="medBooks [spdfEdu]" />
+      <div class="details-Books">
+        <div class="head-box-Books">
+        All MBBS Books
+      </div>
+        All MBBS Books PDF are available here.<br />
+     You can Download it for free.
+      </div>
+    </div><br>
+  </div>
 
     <h3>Tintin Comics Series</h3>
     <h4>Serialwise according to publish year</h4>
