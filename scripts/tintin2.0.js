e34c2525f25ca6369af9da0aa466aafa30902485
spdfEdu <71858133+spdfEdu@users.noreply.github.com>
2022-01-27 11:33:19 +0530

    Add files via upload

---
+++ scripts/tintin2.0.js
@@ -0,0 +1,138 @@
+var  prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("head-box").style.top = "0";
+  } else {
+    if (currentScrollPos > 120) {
+    document.getElementById("head-box").style.top = "-50px";
+    }
+  }
+ prevScrollpos = currentScrollPos;
+}
+//footer section
+{
+var child = document.createElement("div");
+child.innerHTML =
+ `<div id="boxFooter">
+<div id="copyright"><hr>
+<p id="copyrightHead">
+    <b> DMCA Disclaimer </b></p> We are not hosting any copyrighted contents on our servers, it’s a catalog of links that already found on the internet. Check out our 
+<div id="DMCA" onclick="location.href='https://spdfedu.blogspot.com/p/dcma.html'">DMCA Policy</div>.<b>If you feel that we have violated your copyrights, then please 
+<div id="contact" onclick="location.href='mailto:eduspdf@gmail.com?subject=spdfEdu%20Website%20Queries&body=Name%20%3A%20%0D%0AWebsite%20%3A%20%0D%0AEmail%20Id%20%3A%20%0D%0AContact%20No.%20%3A%20%0D%0A%0D%0AMessage%20%3A%20'">contact us</div>
+immediately, concerned content will be taken down within 24 hours.</b><br><br></div>
+  <div id="boxFoot">
+  <div class="foot" onclick="location.href='mailto:eduspdf@gmail.com?subject=spdfEdu%20Website%20Queries&body=Name%20%3A%20%0D%0AWebsite%20%3A%20%0D%0AEmail%20Id%20%3A%20%0D%0AContact%20No.%20%3A%20%0D%0A%0D%0AMessage%20%3A%20'">
+    Contact Us </div> |
+  <div class="foot" onclick="location.href='https://spdfedu.blogspot.com/'">
+   Home </div> |
+  <div class="foot" onclick="location.href='https://spdfedu.blogspot.com/p/dcma.html'"> DCMA Policy
+  </div></div>
+  <div id="copySpdfedu">
+    Copyright &#169; 2021 spdfEdu
+  </div><div class="half-hr"></div>
+</div>`;
+document.body.append(child);
+}
+// shareBox
+var xurl = 'https://bit.ly/tintinComics';
+var url ='Main Website --> bit.ly/spdfEdu';
+var hfs = document.createElement("div");
+hfs.innerHTML =`
+<br><br>
+<div id="share-box">
+    <div id="head-share">
+      Do Share with your friends
+    </div><div id="box">
+       <div class="item">
+<div class="button wp" onclick="location.href='whatsapp://send?text=*'+document.title+'*%0A_'+xurl+'_%0A%0A'+url;" href=''>
+<svg class="shareButtonsSvg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z"/></svg>
+  </div></div>
+       <div class="item">
+  <div class="button sms" onclick="location.href='sms://?body='+document.title+'%0A'+xurl+'%0A%0A'+url" href=''>
+<svg class="shareButtonsSvg" xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 48 48">
+<g transform="translate(0,48) scale(0.05,-0.05)" fill="#000" stroke="none">
+<path d="M100 860 c-19 -19 -20 -33 -20 -402 l0 -383 83 83 82 82 298 0 c284
+0 298 1 317 20 19 19 20 33 20 298 0 278 0 279 -23 300 -23 22 -28 22 -380 22
+-344 0 -358 -1 -377 -20z m700 -300 l0 -240 -302 0 c-266 0 -304 -2 -320 -17
+-17 -16 -18 -7 -18 240 l0 257 320 0 320 0 0 -240z"/></g></svg>
+  </div></div>
+       <div class="item">
+<div class="button cpy" id="copyButton">
+<svg class="shareButtonsSvg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" > 
+<path d="M0 0h24v24H0z" fill="none"/>
+<path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm-1 4l6 6v10c0 1.1-.9 2-2 2H7.99C6.89 23 6 22.1 6 21l.01-14c0-1.1.89-2 1.99-2h7zm-1 7h5.5L14 6.5V12z"/></svg>
+  </div></div>
+       <div class="item">
+<div class="button mail" onclick="location.href='mailto:?subject='+document.title+'&body=Content%20Url:%20'+xurl+'%0A%0A'+url;" href=''>
+<svg class="shareButtonsSvg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48"><path style=" stroke:none;fill-rule:nonzero;fill:rgb(0%,0%,0%);fill-opacity:1;" d="M 44 12 C 44 9.800781 42.199219 8 40 8 L 8 8 C 5.800781 8 4 9.800781 4 12 L 4 36 C 4 38.199219 5.800781 40 8 40 L 40 40 C 42.199219 40 44 38.199219 44 36 Z M 40 12 L 24 21.980469 L 8 12 Z M 40 36 L 8 36 L 8 16 L 24 26 L 40 16 Z M 40 36"/>
+</svg>
+  </div></div>
+       <div class="item">
+ <div class="button shr" onclick="share()">
+ <svg class="shareButtonsSvg" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24">
+<path d="M0 0h24v24H0z" fill="none"/>
+<path d="M18 16.08c-.76 0-1.44.3-1.96.77L8.91 12.7c.05-.23.09-.46.09-.7s-.04-.47-.09-.7l7.05-4.11c.54.5 1.25.81 2.04.81 1.66 0 3-1.34 3-3s-1.34-3-3-3-3 1.34-3 3c0 .24.04.47.09.7L8.04 9.81C7.5 9.31 6.79 9 6 9c-1.66 0-3 1.34-3 3s1.34 3 3 3c.79 0 1.5-.31 2.04-.81l7.12 4.16c-.05.21-.08.43-.08.65 0 1.61 1.31 2.92 2.92 2.92 1.61 0 2.92-1.31 2.92-2.92s-1.31-2.92-2.92-2.92z"/></svg>
+</div></div></div>
+
+  </div><br><br>
+  
+    <div id="teleBox">
+  <div id="tele" onclick="location.href='https://t.me/tintinPDFs'">
+   Connect with us on Telegram&nbsp;&nbsp;
+  <i class="teleSvg"></i></div></div>
+  <br><br>
+
+<div id="moreBooks">
+    <h3 id="head-Books">
+      You may also view:
+    </h3>
+   <div class="box-Books" onclick="location.href='https://bit.ly/ncert-bengali'">
+      <img class="cover-Books" src="https://spdfedu.github.io/image/cover/ncert-bengali-sm.webp" alt="NCERT Books in Bengali | spdfEdu" />
+      <div class="details-Books">
+        <div class="head-box-Books">
+       NCERT in Bengali
+      </div>
+        All NCERT Books PDF in Bengali are available here. <br />
+     From Class I to XII.
+      </div>
+    </div>
+    <div class="box-Books" onclick="location.href='https://bit.ly/medBooks'">
+      <img class="cover-Books" src="https://spdfedu.github.io/image/medBooks-sm.png" alt="medBooks [spdfEdu]" />
+      <div class="details-Books">
+        <div class="head-box-Books">
+        All MBBS Books
+      </div>
+        All MBBS Books PDF are available here.<br />
+     You can Download it for free.
+      </div>
+    </div>
+  </div>`
+var insert = document.getElementById("insert")
+insert.insertBefore(hfs, insert.firstChild);
+//share function
+   function share() {
+   if (navigator.share) {
+       navigator.share({
+       text: document.title+"\n"
+             +xurl+"\n\n"+url})}
+   else {
+     window.open('whatsapp://send?text=*'+document.title+'*%0A_'+xurl+'_%0A%0A'+url)}}
+        
+//copy Button function  
+  var Text = document.createElement('div');
+  Text.id = 'copyTry';
+  Text.innerHTML = document.title +'<br>' +xurl+'<br><br>'+url;
+  document.getElementById('share-box').append(Text);
+  var copyBtn = document.getElementById('copyButton');
+copyBtn.addEventListener('click', function(event) {  
+  var text = document.getElementById('copyTry');
+  var range = document.createRange();  
+  range.selectNode(text);  
+  window.getSelection().addRange(range);
+  try {  
+    var successful = document.execCommand('copy');}
+    catch(err){console.log('Oops, unable to copy')}  
+  window.getSelection().removeAllRanges();
+});
---
+++ scripts/tintinFrame.js
@@ -0,0 +1,55 @@
+//innerHTMLs & srcs & alts
+{
+  document.title = `${ttintin[slNo].eTitle} | spdfEdu`
+  const innerHTMLs = [
+  ['#p1',`<em>${ttintin[slNo].eTitle}</em> (French Title : ${ttintin[slNo].fTitle}) is published in the year of ${ttintin[slNo].year}.`],
+  ['#english', `${ttintin[slNo].eTitle}`],
+  ['#bangla', `${ttintin[slNo].bTitle}<br>${ttintin[slNo].beTitle}`],
+  ['#french', `${ttintin[slNo].fTitle}`],
+  ['#arabic', `${ttintin[slNo].aTitle}`],
+  ['#efname', `${ttintin[slNo].eTitle} in French`],
+  ['#eaname', `${ttintin[slNo].eTitle} in Arabic`],
+  ['#esize', `${ttintin[slNo].eSize} MB`],
+  ['#bsize', `${ttintin[slNo].bSize} MB`],
+  ['#fsize', `${ttintin[slNo].fSize} MB`],
+  ['#asize', `${ttintin[slNo].aSize} MB`],
+             ]
+ innerHTMLs.forEach(([target, content]) => {
+  document.querySelector(target).innerHTML = content;
+  });
+  const srcs = [
+    ['#tc',`${[git]}english/c/${ttintin[slNo].eTitle.toLowerCase().split(' ').join('-')}.webp`,`${ttintin[slNo].eTitle}`],
+    ['#bc',`${[git]}bangla/${ttintin[slNo].beTitle.toLowerCase().split(' ').join('-')}.webp`,`${ttintin[slNo].beTitle}`],
+    ['#fc',`${[git]}french/sm/${ttintin[slNo].fTitle.split(' ').join('-')}.webp`,`${ttintin[slNo].fTitle}`],
+    ['#ac',`${[git]}arabic/sm/${ttintin[slNo].eTitle.toLowerCase().split(' ').join('-')}-arabic.webp`,`${ttintin[slNo].fTitle}`]
+  ]
+  srcs.forEach(([target,src]) => {
+    document.querySelector(target).src = src ;});
+  srcs.forEach(([target,src,alt]) => {
+    document.querySelector(target).alt = alt ;});
+}
+//Book info Box
+{
+var crtDtlBox = document.createElement('div')
+crtDtlBox.innerHTML =`
+<table class="book-detail" border="1"><tr class="info-row"><th class="info-label center" colspan="3">${ttintin[slNo].eTitle}</th></tr><tr class="info-row"><td rowspan="5"><img id="dimg" loading="lazy" class="d-cover" src="${[git]}english/sm/${ttintin[slNo].eTitle.toLowerCase().split(' ').join('-')}.webp"></td></tr><tr class="info-row"><th class="info-label">Creator</th><td class="info-data">Hergé</td></tr><tr class="info-row"><th class="info-label">Publish Year</th><td class="info-data">${ttintin[slNo].year}</td></tr><tr class="info-row"><th class="info-label">Languages Available</th><td class="info-data"><ul class="info-data-ul"><a href="#english"><li>English</li></a><a href="#bangla"><li>Bengali</li></a><a href="#french"><li>French</li></a><a href="#arabic"><li>Arabic</li></a></ul></td></tr><tr class="info-row"><th class="info-label">Category</th><td class="info-data"><em>The Adventures of Tintin</em></td></tr><tr class="info-row"><td class="info-data" colspan="3"><ul class="info-data-ul"><li><span class="info-label">English Title</span><span class="info-data">${ttintin[slNo].eTitle}</span></li><li><span class="info-label">Bengali Title</span><span class="info-data">${ttintin[slNo].bTitle}</span></li><li><span class="info-label">Hindi Title</span><span class="info-data">${ttintin[slNo].hTitle}</span></li><li><span class="info-label">French Title</span><span class="info-data">${ttintin[slNo].fTitle}</span></li><li><span class="info-label">Arabic Title</span><span class="info-data">${ttintin[slNo].aTitle}</span></li></ul></td></tr></table>`;
+document.getElementById("insertBookDetail").append(crtDtlBox)}
+//Inserting Image Swiper & Titles
+{
+ttintin.forEach(ele => {
+  var crtImgSld = document.createElement("div")
+  crtImgSld.className = "swiper-slide"
+  crtImgSld.innerHTML =`
+<a href="/p/${ele.link}.html"><img loading="lazy" src="${[git]}/english/sm/${ele.eTitle.toLowerCase().split(' ').join('-')}.webp" alt="${ele.eTitle}"></a>`
+  var insertSlide = document.getElementById("insertImgSlide")
+  insertSlide.append(crtImgSld);
+
+ var bottomPortion = document.createElement("tr")
+bottomPortion.innerHTML =`
+<td>${ele.sl}</td>
+<td>${ele.year}</td>
+<td onclick="location.href='https://spdfedu.blogspot.com/p/${ele.link}.html'">${ele.eTitle}<br>${ele.bTitle}</td>`
+var insertTitl = document.getElementById("insertTitles")
+insertTitl.append(bottomPortion)
+})}
+$('meta[name="description"]').attr("content", `${ttintin[slNo].eTitle} in different languages. All Tintin Comics available here. Tintin Comics Pdf download`);
