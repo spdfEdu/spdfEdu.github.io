8ce16582c0b83c61ea9b9292a118bf271773f890
spdfEdu <71858133+spdfEdu@users.noreply.github.com>
2022-02-07 08:56:19 +0530

    Add files via upload

---
+++ scripts/stylesheet.js
@@ -0,0 +1,359 @@
+const tdown =
+[
+    {
+        "title": "In the Land of the Soviets",
+        "epgl": "1BDm8aUj-J3x2Yi57oux7pGhV_b0xzhku",
+        "egsdk": "bVJ6aHNVUV9VYnM//qI4McUkd816FPNT3id2nPA",
+        "edgl1": "1KV-zwo_w0Y5sDcrbC-42tk7DgLgm7yg7",
+        "edgl2": "1-p85WQ1DmoU6n3eRMmIpYZ2waf9ENhNa",
+        "bpl": "houe",
+        "bdMega": "4oJkHJiL#cXVppEUGdeaK3brr1ZgZp2YOyYcFD2qJp3L92uvcpmI",
+        "bldMega": "notAvailable",
+        "bdPcloud": "XZoIphXZkwvlg0VdgM5mxKCQCepRWmi71k3X",
+        "fdl": "1RYstAxPXK1Wl-Q_2jzPDFxadcJQvVBns",
+        "adl": "1bR9g7gEqwfMxRxxXc1b92HKsEvkyNAyS"
+    },
+    {
+        "title": "Tintin in the Congo",
+        "epgl": "1RnX5HJwPMIcM2m9j9oWFY7Kb8A4OXdLZ",
+        "egsdk": "bnQ1b05wbldzTnM//TedW_kWRChyPx40X7Mgang",
+        "edgl1": "1aAUnbz9Fa5Mwsc5IfTX5UaUEefjdxIZf",
+        "edgl2": "1KCQBeqZP8BE4hN7QV253BozaFFpdXa4r",
+        "bpl": "xpvf",
+        "bdMega": "M85XmSRI#A-sQLQ1sd6x2Aei8E_Hd9lt3dpZ3_9l5ElSuWn80GHo",
+        "bldMega": "MgoiGLrB#L2qyDujAkoHjjRBpOnq2zX9rO5m59OcGjskwGYhBkKY",
+        "bdPcloud": "XZeNphXZdjjQjyeFKek0A5jvfIYpEjIMWCky",
+        "fdl": "1XRRXMkvUqbf65hzoYO2P6d4cb277bk8c",
+        "adl": "18n68p9lP8il0KFT5sIAdxqfWgqF39BZK"
+    },
+    {
+        "title": "Tintin in America",
+        "epgl": "1o9NF6nOMsfdp7fBEkyKDSDSnxq-RhLwu",
+        "egsdk": "RFp0R1MzYkxIT00//9DyaRjlAhir7oQOuB1oXaw",
+        "edgl1": "1YTxdePzwjHG5q4IfafHBeYt0Yf0098BM",
+        "edgl2": "1_T_7cO1vbY1AEFw1upYgwki2u8iU5Tul",
+        "bpl": "mpet",
+        "bdMega": "01BhnQBK#bx9MhxDq3kaEgkiHMBjZtEN3Z78jgfK195LNT3qu6Rs",
+        "bldMega": "ohYXwa7S#pykyvcc3bzDJf6jfGB0TPkVhWIB-Oo92x9CkgIRiL0c",
+        "bdPcloud": "XZuYW0XZiotselJRM8SXGkyOqVjE1VgV4THy",
+        "fdl": "13A8_GK_6Ny1xWhqVON6iRGufjAnz3-1x",
+        "adl": "1kcpbiirhJXeY98S323UgKiSx0IHcX7ep"
+    },
+    {
+        "title": "Cigars of the Pharaoh",
+        "epgl": "11lFMdU2SIn3BU7-JwJM3qjDBoxq3mQLM",
+        "egsdk": "d2VOdjFPTnRFUDQ//6nfgg7M8Z30OsqXd_xu3ug",
+        "edgl1": "10PS_CAvEbunWvNFdG6EweoIpNvcjkatg",
+        "edgl2": "1T1yg9DbWi4cuxAf6ZYZah70HajHMpdm0",
+        "bpl": "hlgq",
+        "bdMega": "M4IiQbhL#aYLLfX7EMoki9UcBZqslgxoOhEdzQDKoRHrYhxG3WKA",
+        "bldMega": "d0ZkgJhR#3uSH07iiJ7nSl3fbZ3AXqgZPhKjskjEJi-gA9gxqIIU",
+        "bdPcloud": "XZD3phXZzyjHd13iXmjAXWaz8jDyykXF4BM7",
+        "fdl": "1e1tt1pectGwFsQYzgj-M-F9KGqOpgsKy",
+        "adl": "1ncc8mlrbuaGuZvQ2Bd_FOK80vQxor0cI"
+    },
+    {
+        "title": "The Blue Lotus",
+        "epgl": "1HQYEgkQhNTRexFRDadn_VhnL272he_t9",
+        "egsdk": "MGVyWm1QUUVkczg//vD26iNQYlMUr3-WETJ5mMw",
+        "edgl1": "1y1UIIrRSf3xl8ruRyB-QnH_yOxCI7TsG",
+        "edgl2": "1o-SrvlMsm8j-5p4bSGRTZ3Iv2hXVfhnN",
+        "bpl": "cpvw",
+        "bdMega": "IpQxhCxT#J5aVsROyn_LQMqnUX_PsJgq0EdJcgzQg-kwcB1gbTj0",
+        "bldMega": "notAvailable",
+        "bdPcloud": "XZ33phXZjdu4EgVWg2zQburyLRHAf5zAvimy",
+        "fdl": "1KlnhOGBSsxR5xV91yJD6GWSxpvKrnilS",
+        "adl": "15SmFhqECICtXXyPrCjmO9k7bGBQrryAR"
+    },
+    {
+        "title": "The Broken Ear",
+        "epgl": "1nURFa1mGEvNb4Dz_bgEHZXKj2eUAPSlC",
+        "egsdk": "V1Z2eFR3QjVaUzg//vAWlClPb2wdfTwQNxLDhxQ",
+        "edgl1": "1q0cT9YsImeAkiRllQZCV56usfzvi8FUj",
+        "edgl2": "1k-7hB3aVTP1R3NE3fmS4ICGXGtXZXwqz",
+        "bpl": "mhxi",
+        "bdMega": "x5BjnAQT#GS9T1GkkvByqNKh790QHCVXnnD9AxnsehvM15ZWXW_o",
+        "bldMega": "cxBFlSIT#OyyQwDjkx91SRpsVpfXMnrpg-KZ6MJmOB2Wr7dslhp4",
+        "bdPcloud": "XZQGphXZaT9KDToEVqunVuabFpwOFhFx0ep7",
+        "fdl": "11CKrW0Fk4lsE7fNUx6XONtZOS70683JL",
+        "adl": "1zwdFVnZ7-r3cLxHp1yUwdgWEUYz3DnJ9"
+    },
+    {
+        "title": "The Black Island",
+        "epgl": "106iSKE0X7jsvneD1qyXgk9OBeIllmLO9",
+        "egsdk": "TkJTQ21kendVOVk//5mQt2fFOO8TDEzVOpiaeLQ",
+        "edgl1": "1PHYeH0WWFewyLFjMS42LOd_xs8pZMtqe",
+        "edgl2": "1crYS7epoqUXa8Mddb3dSHXmfdDQQDKQ2",
+        "bpl": "cumy",
+        "bdMega": "c1IzzSSJ#Elk7mQCffKMtmfjWjMg7njixlf4b1AAQSGKh0M4izyE",
+        "bldMega": "FponhKIa#OgKEEYPwMGCqezFVpGSP-J3cfuEEsYXuYjTPyKlroEM",
+        "bdPcloud": "XZTGphXZGaPv8s34zWzhjICniomSLzfLuIq7",
+        "fdl": "1gNUz2qlbzPpBIkjlYC5_LslnQCB98svm",
+        "adl": "1kB1-v3tqE7QbBY5_Ct10JeOZzPvvSmQA"
+    },
+    {
+        "title": "King Ottokar's Sceptre",
+        "epgl": "16HgZiiMG_KfuR04LF7wuJOXcSnH99cMG",
+        "egsdk": "UzJxZTFPWGh0ckE//6LXoa2hMVCd5vxkq_y2Y4g",
+        "edgl1": "148US4hbMJ92xTXqVMNQvrCpJLRSDf-jq",
+        "edgl2": "1WAHZlp6nC3jjQCynSwS0J6svkAjcVz-Z",
+        "bpl": "urkk",
+        "bdMega": "x1R3DYga#X9aAlzA9cfr_Te4xERGWIgVRyHRKdocV_UfbTSar_Q4",
+        "bldMega": "notAvailable",
+        "bdPcloud": "XZXvphXZ0xj8zD3zEW43DPYwc2RFLJTt1oOX",
+        "fdl": "1pcImqdfTWzsmGXiCkO9dvXI1AfgP8D2Z",
+        "adl": "1WrHMxcfvNv8RMi9GbQRbCiAJuGvPWpIR"
+    },
+    {
+        "title": "The Crab with the Golden Claws",
+        "epgl": "1GzfBsx8h2YwykaL8RfETfKSIWGfX9D8l",
+        "egsdk": "cWlRTE5VUFp3QXc//e_Pdan9nNw00f1L8PNCmIQ",
+        "edgl1": "13GJEuxFi4W3CneJF790zpLciIFm4ReG0",
+        "edgl2": "1mvakHWsNDrlOTHcvMlkOz1yAzzhCGJzm",
+        "bpl": "xxhh",
+        "bdMega": "YoI3WQDD#Rha2X2NQx97Ul7jfiYmW0TYQL-FuUwJ999l3V-Ui-qE",
+        "bldMega": "notAvailable",
+        "bdPcloud": "XZ2vphXZ9wRrBQbIcrhyLyxyBunTjHcKwrry",
+        "fdl": "1KjsiJy7b7fpWlSf-XGsbWLMybFoHk2RA",
+        "adl": "1WPom3xoRIa1ppcVuQN4GM6KVUFgqEabH"
+    },
+    {
+        "title": "The Shooting Star",
+        "epgl": "1O1MPjonsiBSqEXCjVxS9lgc8L55Kq_p_",
+        "egsdk": "dUlxZEo1dWxjYUE//ia9oom4P-P0bxsCfzsfuDA",
+        "edgl1": "1-w6m8GROVXg3oLdRbyak8YrLjzZSlM1V",
+        "edgl2": "1BtqYeB5ZTqSvmPRAXleFCgAr-lLI9FFU",
+        "bpl": "gogq",
+        "bdMega": "0lxTxYLY#GWnDzyCv9Abd-8iEUEZjpTQpus8r939j1XIIIDhDeiQ",
+        "bldMega": "skgmnYoA#97xmhJTa3o5LEojYUUZTrFBaAvq5Qa4pKg8XO_PRd9Q",
+        "bdPcloud": "XZGvphXZGriYPdGXTSjQ0NmV25bB30StEbSy",
+        "fdl": "1Yq3nlmmA84-bZkAA9gjr2zyoMsKgMIvK",
+        "adl": "1N52bWBYe3hHz8U3LpaUC6lVQZGD6BzM_"
+    },
+    {
+        "title": "The Secret of the Unicorn",
+        "epgl": "13pASTaImah9xwLJsrQpatCFRk2BdgpjR",
+        "egsdk": "dlRzZFVEWWliRDg//8TEg_GwVFXwVjFT-rWPqxw",
+        "edgl1": "1wBXAT-u3KGb_36tRP82SyXbm2q81WzDh",
+        "edgl2": "1VVVj4GLFOQizpRVfF__riDdw2XipRrf7",
+        "bpl": "tvsf",
+        "bdMega": "N0YnDSwJ#MBCt12hzvyrTPZAfwmhCNHzb16AvPLhLt9AEB-bAUIo",
+        "bldMega": "RxpylDCb#3shwPHdBelWwNhafTKBZsyb6kP4Rey4mOT4byUAGilI",
+        "bdPcloud": "XZd3QhXZdhG90baXAH89pVU0mvwljppl6N27",
+        "fdl": "1lAG70nVAwD2XBOqJWLwjDLN_3-PKrRZp",
+        "adl": "1jIgWAbumnWfN-VnXTlucAVyvizUAFyxr"
+    },
+    {
+        "title": "Red Rackham's Treasure",
+        "epgl": "1Egnleb058uOd66qpj19zBw1a59aAEr51",
+        "egsdk": "Ull5SjRXN1ZENFU//pig38ImT2ZVSb65mGtunDw",
+        "edgl1": "1oQRLY2OU8VkgbAuHyRUtKQ-361fAdnSY",
+        "edgl2": "1xsTB7UApIYnK1ciFKFCBVW0te9aUU2zD",
+        "bpl": "spjl",
+        "bdMega": "U5BjgQAY#P8u9PxnRfxbnlVxxmzsPUz80AhNvD1L8jU9tuZXi3T4",
+        "bldMega": "VhQSQBhb#W0viko9_zMv56A0yzLJGmiH0E5k-t_tRFAb_LtnAbnA",
+        "bdPcloud": "XZc3QhXZPhGaOcD1JxjTa2Bavs547uQ7CQEX",
+        "fdl": "13BEsqwEFHBvMaq2UWDifESPPDjiCF731",
+        "adl": "1lqUTHClJR9DqHc7jM8m-VulD5Ao6xM5r"
+    },
+    {
+        "title": "The Seven Crystal Balls",
+        "epgl": "1sVclm-nTCIIxjSvCFYXZoWws_Gdcfqfc",
+        "egsdk": "a044cl9IVDRBY2s//gv3ZbNlwTWVZ8qCImYlVNw",
+        "edgl1": "1aanhJl-AwgTfuRf7X-m50T_M-GRTPavZ",
+        "edgl2": "1sUrszoY681OH_4UkajjIcDlU1Wbmk9cp",
+        "bpl": "pzbx",
+        "bdMega": "5wRVgQqT#Gpw6wklfsbIzdTZbh2q76xQ8i9Rr-378PwrZn_cvsD8",
+        "bldMega": "osABUACL#ojSM-HwM-H_VcyVY9WSdXY6yPFTLh8WCfDzCMvW5nk0",
+        "bdPcloud": "XZzGQhXZv0DHsEUH76RoChxeV6nsEhgh8XSV",
+        "fdl": "1RSKRKDPW7ghcDJxm5XetCUMHfsfjUSuZ",
+        "adl": "16mcz-ATL-NBWhNjrbYc1PZubtw8iecpe"
+    },
+    {
+        "title": "Prisoners of the Sun",
+        "epgl": "1E-Gqi9ucOJt-J1UwlBVQSagaYj0krXMt",
+        "egsdk": "RjVwUUpyWTV4S3M//E1u2IBnxAINK6fGjhbCOKQ",
+        "edgl1": "151y0INbkqsoHNyPW156iefEkJt3oz2Kw",
+        "edgl2": "1Awt8DVMKn0lLoLtUsQOK_5tIZyI-p-kR",
+        "bpl": "aswb",
+        "bdMega": "opByQZiI#bCpO8EkO2xwnZMVY6B5Bujl3jyYG28whEDZVWYX5h_Q",
+        "bldMega": "Zk422b7D#B1e1UCv1t3pkltk0SdqsPTdTBnZ_rISQrAUMAOIj1Zk",
+        "bdPcloud": "XZSOa2XZuSjSMFrv1C4HW97fF1PMUzUPwqFy",
+        "fdl": "1j4Lz__GZfC8iuObriqDIGLQJ0LQUFnR9",
+        "adl": "1eMuzh3eaW971c4ULKSIUNa5hLg34vouw"
+    },
+    {
+        "title": "Land of Black Gold",
+        "epgl": "1eayromClxhvuU-R9WnhJg27FMNTrbj4N",
+        "egsdk": "ZTVLMjFQNkJ0am8//iZPdRJYYPiVdjfLTFkjBUQ",
+        "edgl1": "1WAEM_0Pmy_P92yvuq6eeis1LNSdtPZVt",
+        "edgl2": "1Uid1FovVO2KUnywU8jRdhSZTyDiL9Ufr",
+        "bpl": "ivyj",
+        "bdMega": "9oAVyArB#WUbqiRMlDNyWcde3rQCrwH_J7N5wE3Ic2ReL-4msKIo",
+        "bldMega": "51pCGD7D#r9VfpPTguNzph0WexjMWdIXmeSJ3Z3b_7xH4Oy9zeWE",
+        "bdPcloud": "XZWGQhXZl6EHBCmp9tkG8rF8aumt3h6CoOjk",
+        "fdl": "1mCW_xOWH2H7aWIzwp3spTZ1RiZjdh5yQ",
+        "adl": "1Y0GHL-RGUU90DemTfij4T4NexyyqQKti"
+    },
+    {
+        "title": "Destination Moon",
+        "epgl": "1bEZJTDJeBc41t7-XVzixD5QiZdUBqgvr",
+        "egsdk": "bHVHQjNka1IyWDA//PPUHH8Qmpofb2HsudSP4OA",
+        "edgl1": "1aamQGpbEra8tg_J_GibHRh1oP1eluyUv",
+        "edgl2": "1aMFktnBO2B9W9sNh03wOD3iffeL04NOx",
+        "bpl": "werd",
+        "bdMega": "8tQTXASK#cfKGlln0KoRzTe7b3c8D_gaZRg5JV_65XpkksNHko3k",
+        "bldMega": "QwgXGABD#HOjpdDw7vqwegbsIOul2jGNk2QlUbfhnXydFxA2cbqk",
+        "bdPcloud": "XZ6GQhXZfW4v7gyz88z9JJL8SeznHhDX2bW7",
+        "fdl": "1sxRdbqaYgdOAneoAdq3ePmu_5-ac7WGR",
+        "adl": "1_xQoBsU2aWzXde_P5yzFwWahH2GqvS3-"
+    },
+    {
+        "title": "Explorers on the Moon",
+        "epgl": "1hsTuagoZeSbhoJoH0X76qPoEEL0ZNf_a",
+        "egsdk": "TUJaQ3BDODBHaXM//vlhYueAeozJoAOfmZMjhTA",
+        "edgl1": "1WVaJGsdtS0YkxzM4IdfEbvljj-5WCcaY",
+        "edgl2": "14FMRxxmEv69Bme0kpDwh84kgOXU4G0ex",
+        "bpl": "rgax",
+        "bdMega": "otwUSArL#TCELRzZe3xg3mPtVycPjRUPbEctdl749AZCDoKPBTcA",
+        "bldMega": "Ut5ykSDT#lGZstVSRsgi9aCjrbJMAVXvlRkoTDxVGfhJrEHj8Jfc",
+        "bdPcloud": "XZhvQhXZzA5Ao9IcJzYHYn07PdlYaubJdurk",
+        "fdl": "1Is8PHuZtCc5OE_xIg5GLyylCV0x2baWF",
+        "adl": "1uaPc5-1YmtJK-gaTpnk5D3uFgoQuas9b"
+    },
+    {
+        "title": "The Calculus Affair",
+        "epgl": "1qJpmYXwbICbyTjkV3q_Bno0N_CkeDGH3",
+        "egsdk": "RWhYTUJUOHhfVk0//Nd5t3_oi0uFN2qVBE_JjWA",
+        "edgl1": "157AQI5zTsi3QG1sGIJLen1Th8Oh221e0",
+        "edgl2": "1JzaEZcz80Sskpl55UryPjdcsMxeVlZ3x",
+        "bpl": "ovoq",
+        "bdMega": "c0Q2yB6K#Hn4IKRd7I-229dqJcPZuxT36jAIbOfejtoVP8Gn5nik",
+        "bldMega": "htRCxRpB#KbbmLD04FxPlc6jrdDpoJ0-96Pn2mjKjFV4xZSjCizs",
+        "bdPcloud": "XZPvQhXZkPABzE8RXufUw0oeKf3mfHqgUfsk",
+        "fdl": "1vYU3Zliz3fSYYbfDSfgMDCHVxjjtvvx3",
+        "adl": "1HZpaB_CqO5x9w-OP2HmWdo4Fwm2Wom-x"
+    },
+    {
+        "title": "The Red Sea Sharks",
+        "epgl": "1VVBlHk9D3iF-uimhBzxLzy9nf28Ura0f",
+        "egsdk": "dVpLMXgxQVhlTnM//bMKM1TFRwYU3WlgBwgLVmA",
+        "edgl1": "1eV48XC0GJ1q8Z0unEwRy6kEENybg_e-M",
+        "edgl2": "12d-kWMFUjw-NkpLD29mzx3XtUYbwS97g",
+        "bpl": "ceob",
+        "bdMega": "Ax5yXSyD#IRTfbmbD-c_5gTQuGlmHshKWme4LnOtu5VRIvhMJBuM",
+        "bldMega": "ltoE1DxZ#XlKbCec1a7F8d3Y3cpcGfZlORczex5D35qVJWTO_yMQ",
+        "bdPcloud": "XZ5iQhXZS51qDWbxzljHHaAkvcmNNuPNGw27",
+        "fdl": "1jbHW9fGoZwYLz-H-TARJyHKhUfQm77mv",
+        "adl": "1_HgI_s_6a3hFBUgxr8YG_EcB1ufClape"
+    },
+    {
+        "title": "Tintin in Tibet",
+        "epgl": "12bfN7OPYWtc8WwDcGIclHOPST46LUcIf",
+        "egsdk": "eDZ5NUtqRDIwT2c//0iZ4ljUEYeelLIIw9Srreg",
+        "edgl1": "1CYhLmKD5rl-dI1vuOfCKXx8YCkBW_cji",
+        "edgl2": "1HJfZ5OHtkakG3UhrRXrjg_jm1tCU8YB8",
+        "bpl": "yxzp",
+        "bdMega": "444nkYhQ#Oc7FpXE1YPwif4LFQBl3DCcZphBAC309NHMyEw1N7So",
+        "bldMega": "Jo43iS7K#OJjepT_nnzum4UTUVG7TRPjRr_-fZ6keg8YsYxD5uKA",
+        "bdPcloud": "XZtiQhXZ4xeTykTllBLK5ReCn9sXqSGeBRqX",
+        "fdl": "1ei6ZFZza9mjyJqceB7Vdpdjbo92JPyVM",
+        "adl": "1VDyDK8x2LMajPg7Sl9q1zX5WxKE83aYF"
+    },
+    {
+        "title": "The Castafiore Emerald",
+        "epgl": "1yVkIANq0Hzm4DSXXxo3OgbUwyWuAQDY6",
+        "egsdk": "WWp3QmlwTkpRNXc//P9OpjBvebap9rpWjA1tc-Q",
+        "edgl1": "1oKeIqfyfamjeZA6KLR2rDfbjSLTW8aT7",
+        "edgl2": "1FzFuBHKHO9jwcLyDew-WvozVKeZHWBag",
+        "bpl": "",
+        "bdMega": "Z9IUSJKI#XwsDrQPTWNIYEKKXhw50AUDsVa9_Ja7sI-mxL-SPI0g",
+        "bldMega": "NloyBAwK#Mn8F4_P-4YdmW2Rt8Pbei2Ugbe3B5-FNNB9cGaNSuXo",
+        "bdPcloud": "XZE26YXZBOySSaImWQVctVwkqjT1YhexUfa7",
+        "fdl": "1tIYC4HzM6j0hI08VQxU7Jq4MWum1qvO2",
+        "adl": "18ck3GgvjijicaZSgwFNc5VlIgjwj6OKZ"
+    },
+    {
+        "title": "Flight 714 to Sydney",
+        "epgl": "17pahLFYNdeDxZL7F0NdACx7GP-NfD_UM",
+        "egsdk": "bzdaS1VYNHg4Qzg//CBsCqzhJ1pXVzvoM_jx3Wg",
+        "edgl1": "1ZL955mClhTrTSJnpY0VPqgLUSvhrB0-f",
+        "edgl2": "1rXNqQ2BFEhGkUi_EaTTGBFt4tKyE0R1G",
+        "bpl": "",
+        "bdMega": "p5hDxSJY#fryiCUkGjfArnlW-vRw8QB7gefZn-W3ba4kngLPrW_A",
+        "bldMega": "soAyDZSS#S-ewGX9kiJ4vN9KbbRP628J50KwtGPN8pfJl55CLAio",
+        "bdPcloud": "XZRrQhXZAIcdUihj3HkTMz0y56gbPu5Ywx2X",
+        "fdl": "11dggie7jagr-C6Kw0RvOPGEXClUs98Xj",
+        "adl": "1lIk0ICBWWSGGWicm0Y50DtBNqtCVUsOY"
+    },
+    {
+        "title": "Tintin and the Picaros",
+        "epgl": "16aOhPmqr0zobbBzyDfP24CCxwvnnHApF",
+        "egsdk": "R0dyRnhPdTNULU0//ddEr5T4v5ISSouEp6sxzSQ",
+        "edgl1": "16HL_PX_fXUZykW8Li8h7AB2OnwtrQJ2J",
+        "edgl2": "133TWNG2eHZyNXkZFGGQICtlGCuWvN4Jx",
+        "bpl": "",
+        "bdMega": "dopnkQZb#J7ZkKnD2XdvSySmVFQSQOwAevG0PGIq0-q_HYmAoxtE",
+        "bldMega": "4gw0CTgK#s9jkzUNEOhT2Dw62u1q0nGOlP6sqimOIz3nImuaMUbY",
+        "bdPcloud": "XZ2rQhXZeWuwWdSdwVLypov38K1RKHFXqcHy",
+        "fdl": "1LS4b878y8jFo-TxKFzzKU0A6ihXrXz8E",
+        "adl": "1OsoYTqPrqCEtvYnu0Q5x0pK4Nr2HLqC5"
+    },
+    {
+        "title": "Tintin and Alph-Art",
+        "epgl": "18X7NFZEbFUY4XBWyAUVYbuu5gjUjeYeF",
+        "egsdk": "Rnh5dDU4RzJfSzQ//vcjyICXM99fDi8gP47C3bg",
+        "edgl1": "1cpEx4P71HAt0C2WR2wSn9jgVkS1Kbsie",
+        "edgl2": "1ok_WBkrdJBHSlw7gnOpmhN43AbgMe3GB",
+        "bpl": "jewf",
+        "bdMega": "coQiFTyZ#AOeUATwbVTAudFVk29pw5jLegzh58kFPDl6Gkqa7L5k",
+        "bldMega": "84pllArL#zv3KAWJCMIrhFavKca1bpR-3zzUwKXgs8yzixEEhu7k",
+        "bdPcloud": "XZtDLoXZVydwIaAHzHQ5UCfFO8nun0cndgAX",
+        "fdl": "1vPtcvii-FMcQTFEqdVWT9EYwPMKNPnm4",
+        "adl": "notAvailable"
+    },
+    {
+        "title": "Tintin and the Lake of Sharks",
+        "epgl": "1Jk9sZPJCajNhi7iOHHk3MOxHayyAoT_B",
+        "egsdk": "OUFtaEpqOXFSanM//u-agcJFqggu59_3OKhMhyQ",
+        "edgl1": "1Jk9sZPJCajNhi7iOHHk3MOxHayyAoT_B",
+        "edgl2": "13VuT13kptDCU71WnWasKdFwQPxZVEtgB",
+        "bpl": "qldg",
+        "bdMega": "o0ITWYSK#Kd1uxjUnOvggWa9cXfY1lkkhnKZp6KNZf9yqwQFyveM",
+        "bdPcloud": "XZzcphXZFe5AKDGD4LLNJFUTGdLi1hp6K0MV",
+        "fdl": "19hblpg4LQlh8xsQFdIPX3xNLCHjlIuVj",
+        "adl": "1vi5A5-ME70B-JX_gg2073Heav772rrIp"
+    },
+    {
+        "title": "Bhingroher Agantuk",
+        "bpl": "wyfq",
+        "bdMega": "p4YmAb7C#EfSPXlarWv3WuCpCL77OE0E8uCVVZC0LxXR9amjj0z8",
+        "bdPcloud": "XZIcQhXZvw5yATnMCBBtCEQb2opE2zxP4IR7"
+    }
+]
+const gd = "https://drive.google.com/";
+{
+  
+const hrefs = [
+    ['#evl',gd+"/file/d/0B0Zz72sU0ZZ_"+tdown[slNo].egsdk.split('//').join('/view?usp=drivesdk&resourcekey=0-')],
+    ['#edl1',`${[gd]}uc?export=download&id=${tdown[slNo].edgl1}`],
+    ['#edl2',`${[gd]}uc?export=download&id=${tdown[slNo].edgl2}`],
+    ['#eldl1',`${[gd]}uc?export=download&id=${tdown[slNo].epgl}`],
+    ['#bdl1',`https://mega.nz/file/${tdown[slNo].bdMega}`],
+    ['#bdl2',`https://u.pcloud.link/publink/show?code=${tdown[slNo].bdPcloud}`],
+    ['#bldl1',`https://u.pcloud.link/publink/show?code=${tdown[slNo].bldMega}`],
+    ['#fdlink',`${[gd]}open?id=${tdown[slNo].fdl}`],
+    ['#adlink',`${[gd]}open?id=${tdown[slNo].adl}`]
+  ]
+  hrefs.forEach(([target,href]) => {
+    document.querySelector(target).href = href ;});
+    
+ const dsrcs = [
+    ['#ep',`${[gd]}file/d/${tdown[slNo].epgl}/preview`]
+  ]
+  dsrcs.forEach(([target,src]) => {
+    document.querySelector(target).src = src ;});
+}
+function bp() {
+      document.getElementById("bp").style.display = 'block';
+      document.getElementById("bp").src=`https://online.anyflip.com/bqyps/${tdown[slNo].bpl}/index.html` }
