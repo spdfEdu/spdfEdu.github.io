9a0b8d193f8225411e7837b9448a046e3f9831f2
spdfEdu <71858133+spdfEdu@users.noreply.github.com>
2022-08-04 00:41:03 +0530

    update oldPage.js

--- scripts/oldPage.js
+++ scripts/oldPage.js
@@ -9,24 +9,7 @@ document.body.insertBefore(header, document.body.firstChild);
 //footer section
 var child = document.createElement("div");
 child.innerHTML =
- `<div id="boxFooter">
-<div id="copyright"><hr>
-<p id="copyrightHead">
-    <b> DMCA Disclaimer </b></p> We are not hosting any copyrighted contents on our servers, it’s a catalog of links that already found on the internet. Check out our 
-<div id="DMCA" onclick="location.href='https://spdfedu.blogspot.com/p/dcma.html'">DMCA Policy</div>.<b>If you feel that we have violated your copyrights, then please 
-<div id="contact" onclick="location.href='mailto:eduspdf@gmail.com?subject=spdfEdu%20Website%20Queries&body=Name%20%3A%20%0D%0AWebsite%20%3A%20%0D%0AEmail%20Id%20%3A%20%0D%0AContact%20No.%20%3A%20%0D%0A%0D%0AMessage%20%3A%20'">contact us</div>
-immediately, concerned content will be taken down within 24 hours.</b><br><br></div>
-  <div id="boxFoot">
-  <div class="foot" onclick="location.href='mailto:eduspdf@gmail.com?subject=spdfEdu%20Website%20Queries&body=Name%20%3A%20%0D%0AWebsite%20%3A%20%0D%0AEmail%20Id%20%3A%20%0D%0AContact%20No.%20%3A%20%0D%0A%0D%0AMessage%20%3A%20'">
-    Contact Us </div> |
-  <div class="foot" onclick="location.href='https://spdfedu.blogspot.com/'">
-   Home </div> |
-  <div class="foot" onclick="location.href='https://spdfedu.blogspot.com/p/dcma.html'"> DCMA Policy
-  </div></div>
-  <div id="copySpdfedu">
-    Copyright &#169; 2021 spdfEdu
-  </div><div class="half-hr"></div>
-</div>`;
+ `<div id="boxFooter"><div id="copyright"><hr><p id="copyrightHead"><b> DMCA Disclaimer </b></p> We are not hosting any copyrighted contents on our servers, it’s a catalog of links that already found on the internet. Check out our  <div id="DMCA" onclick="location.href='/p/dcma.html'">DMCA Policy</div>.<b>If you feel that we have violated your copyrights, then please <div id="contact" onclick="location.href='/p/contact-us.html'">contact us</div> immediately, concerned content will be taken down within 24 hours.</b><br><br></div><div id="boxFoot"><div class="foot" onclick="location.href='/p/contact-us.html'"> Contact Us </div> | <div class="foot" onclick="location.href='/'"> Home </div> | <div class="foot" onclick="location.href='/p/dcma.html'"> DCMA Policy</div></div><div id="copySpdfedu"> Copyright &#169; 2021 spdfEdu</div><div class="half-hr"></div></div>`;
 document.body.append(child);
 
 // shareBox
@@ -36,18 +19,7 @@ var srInsert = document.createElement("div");
 srInsert.innerHTML = 
 `<p>If the links are not working, please let us know in the comments section. We will readily update them ASAP.</p>
 <h3 id="moreBooksHeader">You may also See :</h3>
-      <div class="yB">
-<a class="yLink" href="https://spdfedu.blogspot.com/2021/10/1st-Year.html">&#9673; 1st Year MBBS Materials</a>
-      </div>
-      <div class="yB">
-<a class="yLink" href="https://spdfedu.blogspot.com/2021/10/2nd-Year.html">&#9673; 2nd Year MBBS Materials</a>
-      </div>
-      <div class="yB">
-<a class="yLink" href="https://spdfedu.blogspot.com/2021/10/3rd-Year.html">&#9673; 3rd Year MBBS Materials</a>
-      </div>
-      <div class="yB">
-<a class="yLink" href="https://spdfedu.blogspot.com/2021/10/Final-Year.html">&#9673; Final Year MBBS Materials</a>
-      </div>
+<div class="yB"><a class="yLink" href="/2021/10/1st-Year.html">&#9673; 1st Year MBBS Materials</a></div><div class="yB"><a class="yLink" href="/2021/10/2nd-Year.html">&#9673; 2nd Year MBBS Materials</a></div><div class="yB"><a class="yLink" href="/2021/10/3rd-Year.html">&#9673; 3rd Year MBBS Materials</a></div><div class="yB"><a class="yLink" href="/2021/10/Final-Year.html">&#9673; Final Year MBBS Materials</a></div>
       <br><br>
 <div id="share-box">
     <div id="head-share">
