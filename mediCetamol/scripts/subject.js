0d3a6763a224a8fd7c454deb9509d8510fe3c022
spdfEdu <71858133+spdfEdu@users.noreply.github.com>
2022-11-21 13:45:58 +0530

    Code Simplified & Undefined Error Removed

--- mediCetamol/scripts/subject.js
+++ mediCetamol/scripts/subject.js
@@ -1,14 +1,21 @@
 all.forEach(ele => {
 var link = ele.olk || "https://drive.google.com/open?id="+ele.glk,
     hName = ele.h2 || ele.at,
+    ed = ele.ed,
+    sz = ele.sz + "MB",
+    isrc = ele.ilk,
+    atstyle = `style = "display: ${(!ele.at)? 'none' : 'block'}"`,
+    dsstyle = `style = "display: ${(!ele.ds)? 'none' : 'block'}"`,
+    edstyle = `style = "display: ${(!ed)? 'none' : 'inline-block'}"`,
+    szstyle = `style = "display: ${(!ele.sz)? 'none' : 'inline-block'}"`,
     child = document.createElement("div"),
-    isrc = "";
-  if (ele.ilk =="____" || !ele.ilk ) {
-   isrc = "https://lh3.googleusercontent.com/d/" + ele.glk +"=s220?authuser=2";
-  }
+    list = document.createElement("li");
+  
+  if (isrc =="____" || !isrc ) {
+    isrc = "https://lh3.googleusercontent.com/d/" + ele.glk +"=s220?authuser=2"}
   else {
-    isrc = `https://spdfedu.github.io/medBooks/${sub}/sm/${ele.ilk}`;
-  }
+    isrc = `https://spdfedu.github.io/medBooks/${sub}/sm/${isrc}`}
+  
   child.className = "box-book";
   child.innerHTML =`
 <div class="box-flex">
@@ -19,19 +26,18 @@ var link = ele.olk || "https://drive.google.com/open?id="+ele.glk,
   <div class="box-rt">
      <h2><a class="book-link" href="${link}">${hName} ${Sub} Pdf</a></h2>
     <p class="name-detail">${ele.bn}</p>
-    <p class="title-author book-info" style="display: ${(!ele.at)? 'none' : 'inline-block'}">Author : ${(ele.at)}</p>
-    <p style="display: ${(!ele.ds)? 'none' : 'block'}" class="desc">${ele.ds}<br><br></p>
+    <p class="title-author book-info" ${atstyle}>Author : ${(ele.at)}</p>
+    <p ${dsstyle} class="desc">${ele.ds}<br><br></p>
     <p class="book-info">
-   <span class="info-label ed-label"> Edition</span>
-   <span class="info-data edition-data">${ele.ed}</span>
-   <span class="info-label sz-label"> Size</span>
-   <span class="info-data size-data">${ele.sz}MB</span>
+   <span class="info-label ed-label" ${edstyle}> Edition</span>
+   <span class="info-data edition-data" ${edstyle}>${ed}</span>
+   <span class="info-label sz-label" ${szstyle}> Size</span><span class="info-data size-data" ${szstyle}> ${sz}</span>
     </p>
  </div>
 </div>`
   document.getElementById("bs").append(child);
   
-  var list = document.createElement("li")
-  list.innerHTML = `${hName} ${ele.bn} ${ele.ed} edition  pdf download direct link`
+  if (!ed) {ed = "latest"}
+  list.innerHTML = `${hName} ${ele.bn} ${ed} edition  pdf download direct link`
   document.getElementById("helpBrowser").append(list);
 });
