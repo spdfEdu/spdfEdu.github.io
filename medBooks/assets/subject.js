e0dc15ba0c13cc27752000d5363db75dad3f096c
spdfEdu <71858133+spdfEdu@users.noreply.github.com>
2023-02-02 16:06:09 +0530

    Subject JSON Model

---
+++ medBooks/assets/subject.js
@@ -0,0 +1,47 @@
+all.forEach(ele => {
+var link = ele.olk || "https://drive.google.com/open?id="+ele.glk,
+    hName = ele.h2 || ele.at,
+    ed = ele.ed,
+    sz = ele.sz + "MB",
+    isrc = ele.ilk,
+    atstyle = `style = "display: ${(!ele.at)? 'none' : 'block'}"`,
+    edstyle = `style = "display: ${(!ed)? 'none' : 'inline-block'}"`,
+    szstyle = `style = "display: ${(!ele.sz)? 'none' : 'inline-block'}"`,
+    child = document.createElement("div"),
+    list = document.createElement("li");
+  
+  if (!ele.ds) {
+    ele.ds = ""
+  }
+  if (isrc =="____" || !isrc ) {
+    isrc = "https://lh3.googleusercontent.com/d/" + ele.glk +"=s220?authuser=2"}
+  else {
+    isrc = `https://spdfedu.github.io/medBooks/${sub}/sm/${isrc}`}
+  
+  child.className = "box-book";
+  child.innerHTML =`
+<div class="box-flex">
+  <div class="box-lt">
+    <a class="book-link" href="${link}"><img class="cover" loading="lazy" src="${isrc}" alt="${ele.bn} ${hName} ${sub} Pdf" onerror="this.src='https://spdfedu.github.io/medBooks/default-cover.webp'"/>
+    </a>
+  </div>
+  <div class="box-rt">
+    <h2><a class="book-link" href="${link}">${hName} ${Sub} Pdf</a></h2>
+    <p class="name-detail">${ele.bn}</p>
+    <p class="title-author book-info" ${atstyle}>Author : ${(ele.at)}</p>
+    <p class="book-info"> </p>
+    <p class="book-info">
+   <span class="info-label ed-label" ${edstyle}> Edition</span>
+   <span class="info-data edition-data" ${edstyle}>${ed}</span>
+   <span class="info-label sz-label" ${szstyle}> Size</span> <span class="info-data size-data" ${szstyle}>${sz}</span>
+    </p>
+ </div>
+</div>
+<p class="desc">${ele.ds} <br>${hName} ${Sub} Book Pdf Download<br><br></p>
+`
+  document.getElementById("bs").append(child);
+  
+  if (!ed) {ed = "latest"}
+  list.innerHTML = `${hName} ${ele.bn} ${ed} edition  pdf download direct link`
+  document.getElementById("helpBrowser").append(list);
+});
