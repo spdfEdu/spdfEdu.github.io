192e74dfeab0fd59e8ccb0fd46b5ced4c12be8be
spdfEdu <71858133+spdfEdu@users.noreply.github.com>
2023-03-15 21:17:31 +0530

    Book Download New Theme

---
+++ medBooks/assets/book-download.js
@@ -0,0 +1,31 @@
+function download(id) {
+  var dlink = downlink[id],
+      ida = '#'+id+'>a';
+  
+  document.getElementById(id).insertAdjacentHTML("afterend",
+`<h4 id="downText">Please wait while your file is getting ready to download...</h4>
+ <div class="wrap"><div class="spinner"><span id="cdt${id}">10</span></div></div>
+ <h4 id="timerShow">Please wait a few seconds while we are processing your request... </h4>`)
+ 
+  document.querySelector(ida).innerHTML = "PLEASE WAIT ...";
+ 
+  var timeleft = 10 ;
+  var downloadTimer = setInterval(function(){
+        timeleft--;
+    document.getElementById("cdt"+id).textContent = timeleft;
+      if(timeleft <= 0)
+      clearInterval(downloadTimer);
+      },1000);
+      
+  setInterval(function(){
+     document.querySelector(ida).innerHTML = "Link Generated -- Click here"
+     document.querySelector(ida).style.background = "#38c808"
+     document.querySelector(ida).style.color = "#fff"
+     document.querySelector(ida).href = dlink;
+     window.location.href = dlink;
+     if(timeleft <= 0)
+      clearInterval(download);
+      },10000);
+      
+  document.querySelector('#'+id).removeAttribute("onclick");
+      }
