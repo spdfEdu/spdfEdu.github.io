ea05d316e803dc6511e93237454ad6bad1818a88
spdfEdu <71858133+spdfEdu@users.noreply.github.com>
2023-11-06 02:37:22 +0530

    Version 4 subject.js
    Search Added + Download Server Page

--- medBooks/assets/subject.js
+++ medBooks/assets/subject.js
@@ -1,9 +1,18 @@
-all.forEach(ele => {
-var link = "https://drive.google.com/open?id="+ele.glk,
+//Version 4
+//Search + Auto Download Page
+
+const spdfEdu= "spdfEdu";
+all.forEach(createBook);
+
+function createBook(ele, i) {
+  ele.sub = sub;
+  ele.Sub = Sub;
+  var
     hName = ele.h2 || ele.at,
     ed = ele.ed,
     sz = ele.sz + "MB",
-    isrc = `https://spdfedu.github.io/medBooks/${sub}/sm/${ele.ilk}`,
+   isrc = `https://spdfedu.github.io/medBooks/${sub}/sm/${ele.ilk}`,
+   // isrc = "/assets/default-cover.webp",
     atstyle = `style = "display: ${(!ele.at)? 'none' : 'block'}"`,
     edstyle = `style = "display: ${(!ed)? 'none' : 'inline-block'}"`,
     szstyle = `style = "display: ${(!ele.sz)? 'none' : 'inline-block'}"`,
@@ -12,27 +21,32 @@ var link = "https://drive.google.com/open?id="+ele.glk,
     dstxt2 = `${hName} ${Sub} Book Pdf Download<br><br>`,
     dstxt = ele.ds + "<br>" + dstxt2
     ;
-  if (ele.olk) {
-    link = ele.olk
-  }
+    if (ele.olk) {
+      urlCheck = `href='${ele.olk}'`
+    }
+    if (!ele.olk) {
+      downloadBook(i)
+    }
+  
   if (!ele.ds) {
     dstxt = dstxt2
   }
   if (ele.ilk =="____" || !ele.ilk ) {
-    isrc = "https://lh3.googleusercontent.com/d/" + ele.glk +"=s220?authuser=2"}
+  isrc = "https://lh3.googleusercontent.com/d/" + ele.glk +"=s220?authuser=2"
+ }
   
+  child.id = "book"+i
   child.className = "box-book";
   child.innerHTML =`
 <div class="box-flex">
   <div class="box-lt">
-    <a class="book-link" href="${link}"><img class="cover" loading="lazy" src="${isrc}" alt="${ele.bn} ${hName} ${sub} Pdf" onerror="this.src='https://spdfedu.github.io/medBooks/default-cover.webp'"/>
+    <a class="book-link" ${urlCheck}><img class="cover" loading="lazy" src="${isrc}" alt="${ele.bn} ${hName} ${sub} Pdf" onerror="this.src='https://spdfedu.github.io/medBooks/default-cover.webp'"/>
     </a>
   </div>
   <div class="box-rt">
-    <h2><a class="book-link" href="${link}">${hName} ${Sub} Pdf</a></h2>
+    <h2><a class="book-link" ${urlCheck}>${hName} ${Sub} Pdf</a></h2>
     <p class="name-detail">${ele.bn}</p>
-    <p class="title-author book-info" ${atstyle}>Author : ${(ele.at)}</p>
-    <p class="book-info"> </p>
+    <p class="title-author book-info" ${atstyle}>Book by ${(ele.at)}</p>
     <p class="book-info">
    <span class="info-label ed-label" ${edstyle}> Edition</span>
    <span class="info-data edition-data" ${edstyle}>${ed}</span>
@@ -42,34 +56,60 @@ var link = "https://drive.google.com/open?id="+ele.glk,
 </div>
 <p class="desc">${dstxt}</p>
 `
-  document.getElementById("bs").append(child);
-  
-  if (!ed) {ed = "latest"}
+document.getElementById("bs").append(child);
+
+if (!ed) {ed = "latest"}
   list.innerHTML = `${hName} ${ele.bn} ${ed} edition  pdf download direct link`
   document.getElementById("helpBrowser").append(list);
-});
+}
 
-//year section
+function downloadBook(index) {
+  give = all[index]
+  give = JSON.stringify(give)
+  urlCheck = "href='/p/book-download-server.html?m=1" +spdfEdu+easyForward(give)+"'";
+  }
+
+function easyForward(send) {
+  return CryptoJS.AES.encrypt(send, spdfEdu).toString();
+}
+
+bs = document.querySelector('#bs')
+bs.insertAdjacentHTML("beforeBegin", `<input id="searchbar" onkeyup="searchBook()" type="text" name="search" placeholder="Search ${Sub} Books">`);
+
+function searchBook() {
+  let input = document.getElementById('searchbar').value
+  input = encodeURIComponent(input).toLowerCase();
+  bs.innerHTML = ""
+  for (i = 0; i < all.length; i++) {
+    let obj = all[i],
+    h2 = encodeURIComponent(obj.h2).toLowerCase(),
+    bn = encodeURIComponent(obj.bn).toLowerCase(),
+    at = encodeURIComponent(obj.at).toLowerCase()
+    ;
+    if (h2.includes(input) || bn.includes(input) || at.includes(input)) {
+      createBook(obj, i)
+    }
+  }
+}
+
 yrChild  =`
-<h2>You may also See :</h2><div class="yB"><a class="yLink" href="https://spdfedu.blogspot.com/2021/10/1st-Year.html">&#9673; 1st Year MBBS Study Materials</a></div><div class="yB"><a class="yLink" href="https://spdfedu.blogspot.com/2021/10/2nd-Year.html">&#9673; 2nd Year MBBS Study Materials</a></div><div class="yB"><a class="yLink" href="https://spdfedu.blogspot.com/2021/10/3rd-Year.html">&#9673; 3rd Year MBBS Study Materials</a></div><div class="yB"><a class="yLink" href="https://spdfedu.blogspot.com/2021/10/Final-Year.html">&#9673; Final Year MBBS Study Materials</a></div>`;
+<h2>You may also See :</h2><div class="yB"><a class="yLink" href="https://spdfedu.blogspot.com/2021/10/1st-Year.html">1st Year MBBS Study Materials</a></div><div class="yB"><a class="yLink" href="https://spdfedu.blogspot.com/2021/10/2nd-Year.html">2nd Year MBBS Study Materials</a></div><div class="yB"><a class="yLink" href="https://spdfedu.blogspot.com/2021/10/3rd-Year.html">3rd Year MBBS Study Materials</a></div><div class="yB"><a class="yLink" href="https://spdfedu.blogspot.com/2021/10/Final-Year.html">Final Year MBBS Study Materials</a></div>`;
 document.getElementById('moreBooks').insertAdjacentHTML('afterend',yrChild)
 
-// shareBox
 var xurl =window.location.href.split("?m=1")[0]
 var url ='Main Website --> bit.ly/spdfEdu'
 document.getElementById('box').insertAdjacentHTML('afterend',
 `<br><div id="share-box"><div id="head-share">Do Share with your friends</div><div id="box"><div class="item"><div class="button wp" onclick="location.href='whatsapp://send?text=*'+document.title+'*%0A_'+xurl+'_%0A%0A'+url;" href=''><i class="wpsvg ssvg"></i></div></div><div class="item"><div class="button sms" onclick="location.href='sms:?body='+document.title+'%0A'+xurl+'%0A%0A'+url" href=''><i class="smssvg ssvg"></i></div></div><div class="item"><div class="button cpy" id="copyButton"><i class="copysvg ssvg"></i></div></div><div class="item"><div class="button mail" onclick="location.href='mailto:?subject='+document.title+'&body=Content%20Url:%20'+xurl+'%0A%0A'+url;" href=''><i class="mailsvg ssvg"></i></div></div><div class="item"><div class="button shr" onclick="share()"><i class="shrsvg ssvg"></i></div></div></div></div>`);
 
-//share function
-   function share() {
+function share() {
    if (navigator.share) {
        navigator.share({
        text: document.title+"\n"
              +xurl+"\n\n"+url})}
    else {
-     window.open('whatsapp://send?text=*'+document.title+'*%0A_'+xurl+'_%0A%0A'+url)}}
+     window.open('whatsapp://send?text=*'+document.title+'*%0A_'+xurl+'_%0A%0A'+url)}
+}
         
-//copy Button function  
   var Text = document.createElement('div');
   Text.id = 'copyTry';
   Text.innerHTML = document.title +'<br>' +xurl+'<br><br>'+url;
@@ -84,4 +124,4 @@ copyBtn.addEventListener('click', function(event) {
     var successful = document.execCommand('copy');}
     catch(err){console.log('Oops, unable to copy')}  
   window.getSelection().removeAllRanges();
-})
+}
