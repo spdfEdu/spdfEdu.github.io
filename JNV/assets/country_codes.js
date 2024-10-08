c2f388e88a718bfc75550f1ee6d0e8531b326f0a
spdfEdu <71858133+spdfEdu@users.noreply.github.com>
2024-10-08 15:12:37 +0530

    Create country_codes.js

---
+++ JNV/assets/country_codes.js
@@ -0,0 +1,728 @@
+countryCodes = [{
+  n: "Afghanistan",
+  c: 93
+}, {
+  n: "Aland Islands",
+  c: 358
+}, {
+  n: "Albania",
+  c: 355
+}, {
+  n: "Algeria",
+  c: 213
+}, {
+  n: "AmericanSamoa",
+  c: 1684
+}, {
+  n: "Andorra",
+  c: 376
+}, {
+  n: "Angola",
+  c: 244
+}, {
+  n: "Anguilla",
+  c: 1264
+}, {
+  n: "Antarctica",
+  c: 672
+}, {
+  n: "Antigua and Barbuda",
+  c: 1268
+}, {
+  n: "Argentina",
+  c: 54
+}, {
+  n: "Armenia",
+  c: 374
+}, {
+  n: "Aruba",
+  c: 297
+}, {
+  n: "Australia",
+  c: 61
+}, {
+  n: "Austria",
+  c: 43
+}, {
+  n: "Azerbaijan",
+  c: 994
+}, {
+  n: "Bahamas",
+  c: 1242
+}, {
+  n: "Bahrain",
+  c: 973
+}, {
+  n: "Bangladesh",
+  c: 880
+}, {
+  n: "Barbados",
+  c: 1246
+}, {
+  n: "Belarus",
+  c: 375
+}, {
+  n: "Belgium",
+  c: 32
+}, {
+  n: "Belize",
+  c: 501
+}, {
+  n: "Benin",
+  c: 229
+}, {
+  n: "Bermuda",
+  c: 1441
+}, {
+  n: "Bhutan",
+  c: 975
+}, {
+  n: "Bolivia, Plurinational State of",
+  c: 591
+}, {
+  n: "Bosnia and Herzegovina",
+  c: 387
+}, {
+  n: "Botswana",
+  c: 267
+}, {
+  n: "Brazil",
+  c: 55
+}, {
+  n: "British Indian Ocean Territory",
+  c: 246
+}, {
+  n: "Brunei Darussalam",
+  c: 673
+}, {
+  n: "Bulgaria",
+  c: 359
+}, {
+  n: "Burkina Faso",
+  c: 226
+}, {
+  n: "Burundi",
+  c: 257
+}, {
+  n: "Cambodia",
+  c: 855
+}, {
+  n: "Cameroon",
+  c: 237
+}, {
+  n: "Canada",
+  c: 1
+}, {
+  n: "Cape Verde",
+  c: 238
+}, {
+  n: "Cayman Islands",
+  c: 345
+}, {
+  n: "Central African Republic",
+  c: 236
+}, {
+  n: "Chad",
+  c: 235
+}, {
+  n: "Chile",
+  c: 56
+}, {
+  n: "China",
+  c: 86
+}, {
+  n: "Christmas Island",
+  c: 61
+}, {
+  n: "Cocos (Keeling) Islands",
+  c: 61
+}, {
+  n: "Colombia",
+  c: 57
+}, {
+  n: "Comoros",
+  c: 269
+}, {
+  n: "Congo",
+  c: 242
+}, {
+  n: "Congo, The Democratic Republic of the Congo",
+  c: 243
+}, {
+  n: "Cook Islands",
+  c: 682
+}, {
+  n: "Costa Rica",
+  c: 506
+}, {
+  n: "Cote d'Ivoire",
+  c: 225
+}, {
+  n: "Croatia",
+  c: 385
+}, {
+  n: "Cuba",
+  c: 53
+}, {
+  n: "Cyprus",
+  c: 357
+}, {
+  n: "Czech Republic",
+  c: 420
+}, {
+  n: "Denmark",
+  c: 45
+}, {
+  n: "Djibouti",
+  c: 253
+}, {
+  n: "Dominica",
+  c: 1767
+}, {
+  n: "Dominican Republic",
+  c: 1849
+}, {
+  n: "Ecuador",
+  c: 593
+}, {
+  n: "Egypt",
+  c: 20
+}, {
+  n: "El Salvador",
+  c: 503
+}, {
+  n: "Equatorial Guinea",
+  c: 240
+}, {
+  n: "Eritrea",
+  c: 291
+}, {
+  n: "Estonia",
+  c: 372
+}, {
+  n: "Ethiopia",
+  c: 251
+}, {
+  n: "Falkland Islands (Malvinas)",
+  c: 500
+}, {
+  n: "Faroe Islands",
+  c: 298
+}, {
+  n: "Fiji",
+  c: 679
+}, {
+  n: "Finland",
+  c: 358
+}, {
+  n: "France",
+  c: 33
+}, {
+  n: "French Guiana",
+  c: 594
+}, {
+  n: "French Polynesia",
+  c: 689
+}, {
+  n: "Gabon",
+  c: 241
+}, {
+  n: "Gambia",
+  c: 220
+}, {
+  n: "Georgia",
+  c: 995
+}, {
+  n: "Germany",
+  c: 49
+}, {
+  n: "Ghana",
+  c: 233
+}, {
+  n: "Gibraltar",
+  c: 350
+}, {
+  n: "Greece",
+  c: 30
+}, {
+  n: "Greenland",
+  c: 299
+}, {
+  n: "Grenada",
+  c: 1473
+}, {
+  n: "Guadeloupe",
+  c: 590
+}, {
+  n: "Guam",
+  c: 1671
+}, {
+  n: "Guatemala",
+  c: 502
+}, {
+  n: "Guernsey",
+  c: 44
+}, {
+  n: "Guinea",
+  c: 224
+}, {
+  n: "Guinea-Bissau",
+  c: 245
+}, {
+  n: "Guyana",
+  c: 595
+}, {
+  n: "Haiti",
+  c: 509
+}, {
+  n: "Holy See (Vatican City State)",
+  c: 379
+}, {
+  n: "Honduras",
+  c: 504
+}, {
+  n: "Hong Kong",
+  c: 852
+}, {
+  n: "Hungary",
+  c: 36
+}, {
+  n: "Iceland",
+  c: 354
+}, {
+  n: "India",
+  c: 91
+}, {
+  n: "Indonesia",
+  c: 62
+}, {
+  n: "Iran, Islamic Republic of Persian Gulf",
+  c: 98
+}, {
+  n: "Iraq",
+  c: 964
+}, {
+  n: "Ireland",
+  c: 353
+}, {
+  n: "Isle of Man",
+  c: 44
+}, {
+  n: "Israel",
+  c: 972
+}, {
+  n: "Italy",
+  c: 39
+}, {
+  n: "Jamaica",
+  c: 1876
+}, {
+  n: "Japan",
+  c: 81
+}, {
+  n: "Jersey",
+  c: 44
+}, {
+  n: "Jordan",
+  c: 962
+}, {
+  n: "Kazakhstan",
+  c: 77
+}, {
+  n: "Kenya",
+  c: 254
+}, {
+  n: "Kiribati",
+  c: 686
+}, {
+  n: "Korea, Democratic People's Republic of Korea",
+  c: 850
+}, {
+  n: "Korea, Republic of South Korea",
+  c: 82
+}, {
+  n: "Kuwait",
+  c: 965
+}, {
+  n: "Kyrgyzstan",
+  c: 996
+}, {
+  n: "Laos",
+  c: 856
+}, {
+  n: "Latvia",
+  c: 371
+}, {
+  n: "Lebanon",
+  c: 961
+}, {
+  n: "Lesotho",
+  c: 266
+}, {
+  n: "Liberia",
+  c: 231
+}, {
+  n: "Libyan Arab Jamahiriya",
+  c: 218
+}, {
+  n: "Liechtenstein",
+  c: 423
+}, {
+  n: "Lithuania",
+  c: 370
+},
+  {
+    n: "Luxembourg",
+    c: 352
+  }, {
+    n: "Macao",
+    c: 853
+  }, {
+    n: "Macedonia",
+    c: 389
+  }, {
+    n: "Madagascar",
+    c: 261
+  }, {
+    n: "Malawi",
+    c: 265
+  }, {
+    n: "Malaysia",
+    c: 60
+  }, {
+    n: "Maldives",
+    c: 960
+  }, {
+    n: "Mali",
+    c: 223
+  }, {
+    n: "Malta",
+    c: 356
+  }, {
+    n: "Marshall Islands",
+    c: 692
+  }, {
+    n: "Martinique",
+    c: 596
+  }, {
+    n: "Mauritania",
+    c: 222
+  }, {
+    n: "Mauritius",
+    c: 230
+  }, {
+    n: "Mayotte",
+    c: 262
+  }, {
+    n: "Mexico",
+    c: 52
+  }, {
+    n: "Micronesia, Federated States of Micronesia",
+    c: 691
+  }, {
+    n: "Moldova",
+    c: 373
+  }, {
+    n: "Monaco",
+    c: 377
+  }, {
+    n: "Mongolia",
+    c: 976
+  }, {
+    n: "Montenegro",
+    c: 382
+  }, {
+    n: "Montserrat",
+    c: 1664
+  }, {
+    n: "Morocco",
+    c: 212
+  }, {
+    n: "Mozambique",
+    c: 258
+  }, {
+    n: "Myanmar",
+    c: 95
+  }, {
+    n: "Namibia",
+    c: 264
+  }, {
+    n: "Nauru",
+    c: 674
+  }, {
+    n: "Nepal",
+    c: 977
+  }, {
+    n: "Netherlands",
+    c: 31
+  }, {
+    n: "Netherlands Antilles",
+    c: 599
+  }, {
+    n: "New Caledonia",
+    c: 687
+  }, {
+    n: "New Zealand",
+    c: 64
+  }, {
+    n: "Nicaragua",
+    c: 505
+  }, {
+    n: "Niger",
+    c: 227
+  }, {
+    n: "Nigeria",
+    c: 234
+  }, {
+    n: "Niue",
+    c: 683
+  }, {
+    n: "Norfolk Island",
+    c: 672
+  }, {
+    n: "Northern Mariana Islands",
+    c: 1670
+  }, {
+    n: "Norway",
+    c: 47
+  }, {
+    n: "Oman",
+    c: 968
+  }, {
+    n: "Pakistan",
+    c: 92
+  }, {
+    n: "Palau",
+    c: 680
+  }, {
+    n: "Palestinian Territory, Occupied",
+    c: 970
+  }, {
+    n: "Panama",
+    c: 507
+  }, {
+    n: "Papua New Guinea",
+    c: 675
+  }, {
+    n: "Paraguay",
+    c: 595
+  }, {
+    n: "Peru",
+    c: 51
+  }, {
+    n: "Philippines",
+    c: 63
+  }, {
+    n: "Pitcairn",
+    c: 872
+  }, {
+    n: "Poland",
+    c: 48
+  }, {
+    n: "Portugal",
+    c: 351
+  }, {
+    n: "Puerto Rico",
+    c: 1939
+  }, {
+    n: "Qatar",
+    c: 974
+  }, {
+    n: "Romania",
+    c: 40
+  }, {
+    n: "Russia",
+    c: 7
+  }, {
+    n: "Rwanda",
+    c: 250
+  }, {
+    n: "Reunion",
+    c: 262
+  }, {
+    n: "Saint Barthelemy",
+    c: 590
+  }, {
+    n: "Saint Helena, Ascension and Tristan Da Cunha",
+    c: 290
+  }, {
+    n: "Saint Kitts and Nevis",
+    c: 1869
+  }, {
+    n: "Saint Lucia",
+    c: 1758
+  }, {
+    n: "Saint Martin",
+    c: 590
+  }, {
+    n: "Saint Pierre and Miquelon",
+    c: 508
+  }, {
+    n: "Saint Vincent and the Grenadines",
+    c: 1784
+  }, {
+    n: "Samoa",
+    c: 685
+  }, {
+    n: "San Marino",
+    c: 378
+  }, {
+    n: "Sao Tome and Principe",
+    c: 239
+  }, {
+    n: "Saudi Arabia",
+    c: 966
+  }, {
+    n: "Senegal",
+    c: 221
+  }, {
+    n: "Serbia",
+    c: 381
+  }, {
+    n: "Seychelles",
+    c: 248
+  }, {
+    n: "Sierra Leone",
+    c: 232
+  }, {
+    n: "Singapore",
+    c: 65
+  }, {
+    n: "Slovakia",
+    c: 421
+  }, {
+    n: "Slovenia",
+    c: 386
+  }, {
+    n: "Solomon Islands",
+    c: 677
+  }, {
+    n: "Somalia",
+    c: 252
+  }, {
+    n: "South Africa",
+    c: 27
+  }, {
+    n: "South Sudan",
+    c: 211
+  }, {
+    n: "South Georgia and the South Sandwich Islands",
+    c: 500
+  }, {
+    n: "Spain",
+    c: 34
+  }, {
+    n: "Sri Lanka",
+    c: 94
+  }, {
+    n: "Sudan",
+    c: 249
+  }, {
+    n: "Suriname",
+    c: 597
+  }, {
+    n: "Svalbard and Jan Mayen",
+    c: 47
+  }, {
+    n: "Swaziland",
+    c: 268
+  }, {
+    n: "Sweden",
+    c: 46
+  }, {
+    n: "Switzerland",
+    c: 41
+  }, {
+    n: "Syrian Arab Republic",
+    c: 963
+  }, {
+    n: "Taiwan",
+    c: 886
+  }, {
+    n: "Tajikistan",
+    c: 992
+  }, {
+    n: "Tanzania, United Republic of Tanzania",
+    c: 255
+  }, {
+    n: "Thailand",
+    c: 66
+  }, {
+    n: "Timor-Leste",
+    c: 670
+  }, {
+    n: "Togo",
+    c: 228
+  }, {
+    n: "Tokelau",
+    c: 690
+  }, {
+    n: "Tonga",
+    c: 676
+  }, {
+    n: "Trinidad and Tobago",
+    c: 1868
+  }, {
+    n: "Tunisia",
+    c: 216
+  }, {
+    n: "Turkey",
+    c: 90
+  }, {
+    n: "Turkmenistan",
+    c: 993
+  }, {
+    n: "Turks and Caicos Islands",
+    c: 1649
+  }, {
+    n: "Tuvalu",
+    c: 688
+  }, {
+    n: "Uganda",
+    c: 256
+  }, {
+    n: "Ukraine",
+    c: 380
+  }, {
+    n: "United Arab Emirates",
+    c: 971
+  }, {
+    n: "United Kingdom",
+    c: 44
+  }, {
+    n: "United States",
+    c: 1
+  }, {
+    n: "Uruguay",
+    c: 598
+  }, {
+    n: "Uzbekistan",
+    c: 998
+  }, {
+    n: "Vanuatu",
+    c: 678
+  }, {
+    n: "Venezuela, Bolivarian Republic of Venezuela",
+    c: 58
+  }, {
+    n: "Vietnam",
+    c: 84
+  }, {
+    n: "Virgin Islands, British",
+    c: 1284
+  }, {
+    n: "Virgin Islands, U.S.",
+    c: 1340
+  }, {
+    n: "Wallis and Futuna",
+    c: 681
+  }, {
+    n: "Yemen",
+    c: 967
+  }, {
+    n: "Zambia",
+    c: 260
+  }, {
+    n: "Zimbabwe",
+    c: 263
+  }]
